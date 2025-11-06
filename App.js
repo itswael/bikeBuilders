275e7c77e737d1dc7380749587e5a7ac854bae99
mohammad wael <mohd.wael.cs.2016@miet.ac.in>
2025-11-06 14:23:23 -0500

    updated configurations

--- App.js
+++ App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
-import { Provider as PaperProvider } from 'react-native-paper';
+import { Provider as PaperProvider, MD3LightTheme } from 'react-native-paper';
 import { AppProvider } from './src/context/AppContext';
 import Navigation from './src/navigation/Navigation';
+
+// Force light theme
+const theme = {
+  ...MD3LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    primary: '#6200ee',
+    secondary: '#03dac4',
+  },
+};
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <PaperProvider>
+      <PaperProvider theme={theme}>
         <AppProvider>
           <Navigation />
-          <StatusBar style="light" />
+          <StatusBar style="dark" />
         </AppProvider>
       </PaperProvider>
     </GestureHandlerRootView>
